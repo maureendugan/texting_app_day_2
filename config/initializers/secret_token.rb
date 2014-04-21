61d8327ff7e1eaba0fe6d89d4fb755182c0496e3
Patrick McGreevy & Josh Fowler <student@epicodus.com>
2014-04-21 10:48:45 -0700

    initial commit, migrate to users table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Texting::Application.config.secret_key_base = '248bf395485efac5a48be8e2e65c86a4e1bbd26424b82f15cd4b9809aa3cf25c15608dd74d57840faa764a3ad10a7dff4077c424e99c725d3fd647191ca7955e'
